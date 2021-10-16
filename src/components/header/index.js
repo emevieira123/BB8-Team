85248f04ec9c5541b2bcf1fa1c7ac74bafb5b58f
Emerson Vieira <emevieira@gmail.com>
2021-10-16 14:57:36 -0400

    implementacao do header do projeto

---
+++ src/components/header/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.css';
+import Logo from '../../assets/Logo.svg';
+
+const Header = () => {
+    return (
+        <div className="container">
+            <div className="logo">
+                <img src={Logo} alt="" />
+                <h2>BB8 Team</h2>
+            </div>
+            <div className="menu">
+                <button className="btnMenu" href="http://localhost:3000/">Contact</button>
+                <button className="btnMenu" href="http://localhost:3000/">Register</button>
+                <button className="btnLogin" href="http://localhost:3000/">Log In</button>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
